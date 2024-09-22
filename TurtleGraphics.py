067eff4abf4a4bcd20be3c34d94282c8d9deae3e
aaliyahmoore <aaliyahmoore@unomaha.edu>
2024-09-22 14:42:06 -0500

    done with lab 4

--- TurtleGraphics.py
+++ TurtleGraphics.py
@@ -11,13 +11,32 @@ def drawSquare(myTurtle, size):
         myTurtle.forward(size)
         myTurtle.right(90)
 
+def drawPolygon(bob, sides):
+    for s in range(sides):
+        bob.forward(50)
+        bob.right(360/sides)
 
+def fillCorner(alice, corner): 
+    drawSquare (alice, 100)
+    
+    if corner ==1: 
+        alice.begin_fill()
+        drawSquare(alice, 50)
+        alice.end_fill()
+    elif corner == 2: 
+      alice.forward(50)
+      alice.begin_fill()
+      drawSquare(alice, 50)
+      alice.end_fill()
+    
 def main():
     myTurtle = turtle.Turtle()
-    # drawPolygon(myTurtle, 5) #draws a pentagon
+    
+    #drawSquare(myTurtle, 5)
+    #drawPolygon(myTurtle, 8) #draws a pentagon
     # drawPolygon(myTurtle, 8) #draws an octogon
 
-    # fillCorner(myTurtle, 2) #draws a square with top right corner filled in.
+    fillCorner(myTurtle, 2) #draws a square with top right corner filled in.
     # fillCorner(myTurtle, 3) #draws a square bottom left corner filled in.
 
     # squaresInSquares(myTurtle, 5) #draws 5 concentric squares
